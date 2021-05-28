a5268878c1797077b126fff369064d032c4cf5ad
sebuelias <sebuelias27@gmail.com>
2021-05-29 01:18:58 +0530

    Added search input and logic based on username

--- components/pages/index.js
+++ components/pages/index.js
@@ -1,13 +1,36 @@
 import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
 import MemberList from 'components/members-list';
 import classNames from './home-page.module.scss';
 
 const HomePage = ({ membersArr }) => {
+  const [searchKey, setSearchKey] = useState('');
+  const [filteredMembersArray, setFilteredMembersArray] = useState([...membersArr]);
+
+  const handleSearch = (event) => {
+    setSearchKey(event.target.value.toLowerCase());
+  };
+
+  useEffect(() => {
+    let filteredMembers = membersArr.filter((member) => {
+      return member.username.toLowerCase().includes(searchKey);
+    });
+
+    setFilteredMembersArray([...filteredMembers]);
+  }, [searchKey]);
+
   return (
     <div className={classNames.container}>
       <img className={classNames.img} src="/images/Real-Dev-Squad@1x.png" alt="real-dev squad" />
       <h1 className={classNames.heading}>Real Dev Squad Members</h1>
-      <MemberList membersArr={membersArr} />
+      <input
+        className={classNames.input}
+        type="search"
+        placeholder="Search"
+        value={searchKey}
+        onChange={handleSearch}
+      />
+      <MemberList membersArr={filteredMembersArray} />
     </div>
   );
 };
